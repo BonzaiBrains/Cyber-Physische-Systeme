3850684ade6101a75557e378e2ee1a68b8bc737b
BonzaiBrains <133261117+BonzaiBrains@users.noreply.github.com>
2024-04-17 11:03:57 +0200

    Update main.py

--- Luftzirkulation-Steuerung/main.py
+++ Luftzirkulation-Steuerung/main.py
@@ -1,33 +1,113 @@
-import sys
+import time
+import board
+import adafruit_dht
+import paho.mqtt.client as mqtt
+import RPi.GPIO as GPIO
 
-import Adafruit_DHT
+# Initialisiere den DHT22-Sensor
+dhtDevice = adafruit_dht.DHT22(board.D4, use_pulseio=False)
+
+# MQTT-Verbindungsinformationen
+mqtt_broker = "172.17.0.102"
+mqtt_topic = "Group5/"
+
+GPIO.setmode(GPIO.BCM)
+# PIN-Zuweisung am Raspberry
+A=18
+B=23
+C=24
+D=25
+time = 0.001
+
+# PINS definieren
+GPIO.setup(A,GPIO.OUT)
+GPIO.setup(B,GPIO.OUT)
+GPIO.setup(C,GPIO.OUT)
+GPIO.setup(D,GPIO.OUT)
+GPIO.output(A, False)
+GPIO.output(B, False)
+GPIO.output(C, False)
+GPIO.output(D, False)
+
+# Callback für die Verbindung zum MQTT-Broker
+def on_connect(client, userdata, flags, rc):
+    print("Verbunden mit MQTT-Broker. Result Code: " + str(rc))
+
+# Initialisiere den MQTT-Client
+client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
+client.on_connect = on_connect
+
+# Verbinde dich mit dem MQTT-Broker
+client.connect(mqtt_broker, 1883, 60)
+
+# Ansteuerung der Spulen des Motors
+def Step1():
+    GPIO.output(D, True)
+    sleep (time)
+    GPIO.output(D, False)
+
+def Step2():
+    GPIO.output(D, True)
+    GPIO.output(C, True)
+    sleep (time)
+    GPIO.output(D, False)
+    GPIO.output(C, False)
+
+def Step3():
+    GPIO.output(C, True)
+    sleep (time)
+    GPIO.output(C, False)
+def Step4():
+    GPIO.output(B, True)
+    GPIO.output(C, True)
+    sleep (time)
+    GPIO.output(B, False)
+    GPIO.output(C, False)
+
+def Step5():
+    GPIO.output(B, True)
+    sleep (time)
+    GPIO.output(B, False)
+
+def Step6():
+    GPIO.output(A, True)
+    GPIO.output(B, True)
+    sleep (time)
+    GPIO.output(A, False)
+    GPIO.output(B, False)
+
+def Step7():
+    GPIO.output(A, True)
+    sleep (time)
+    GPIO.output(A, False)
+
+def Step8():
+    GPIO.output(D, True)
+    GPIO.output(A, True)
+    sleep (time)
+    GPIO.output(D, False)
+    GPIO.output(A, False)
 
 
-# Parse command line parameters.
-sensor_args = { '11': Adafruit_DHT.DHT11,
-                '22': Adafruit_DHT.DHT22,
-                '2302': Adafruit_DHT.AM2302 }
-if len(sys.argv) == 3 and sys.argv[1] in sensor_args:
-    sensor = sensor_args[sys.argv[1]]
-    pin = sys.argv[2]
-else:
-    print('Usage: sudo ./main.py [11|22|2302] <GPIO pin number>')
-    print('Example: sudo ./main.py 2302 4 - Read from an AM2302 connected to GPIO pin #4')
-    sys.exit(1)
+while True:
+    try:
+        # Lese die Werte vom DHT22-Sensor aus
+        temperature_c = dhtDevice.temperature
+        humidity = dhtDevice.humidity
+        
+        print(temperature_c)
+        print(humidity)
+        # Sende die Werte über MQTT
+        #client.publish(mqtt_topic+"Temp", payload=f"{temperature_c}", qos=0)
+        #client.publish(mqtt_topic+"Humid", payload=f"{humidity}", qos=0)
+        
+    except RuntimeError as error:
+        print(error.args[0])
+        time.sleep(2.0)
+        continue
+    except Exception as error:
+        dhtDevice.exit()
+        raise error
 
-# Try to grab a sensor reading.  Use the read_retry method which will retry up
-# to 15 times to get a sensor reading (waiting 2 seconds between each retry).
-humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
+    time.sleep(5.0)
 
-# Un-comment the line below to convert the temperature to Fahrenheit.
-# temperature = temperature * 9/5.0 + 32
-
-# Note that sometimes you won't get a reading and
-# the results will be null (because Linux can't
-# guarantee the timing of calls to read the sensor).
-# If this happens try again!
-if humidity is not None and temperature is not None:
-    print('Temp={0:0.1f}*  Humidity={1:0.1f}%'.format(temperature, humidity))
-else:
-    print('Failed to get reading. Try again!')
-    sys.exit(1)
